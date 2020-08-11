5c44727f2c3f983912a2b128c8aab95faa18ddea
Jed Stanton <jedstanton@hotmail.com>
2020-08-11 14:38:48 +0100

    Speed drop down added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Game from "./components/game";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Game />
-    </div>
-  );
+class App extends Component {
+  state = {
+    speed: 1000,
+  };
+
+  // handleChange = () => {
+  //   this.setState({ speed: event.target.value });
+  // };
+  render() {
+    return (
+      <div className="App">
+        <Game />
+        <label for="speed">Select Speed:</label>
+
+        <select onChange={this.handleChange} name="speed" id="speed">
+          <option value="500">Slow</option>
+          <option value="1000" selected="selected">
+            Medium
+          </option>
+          <option value="2000">Fast</option>
+        </select>
+      </div>
+    );
+  }
 }
 
 export default App;
